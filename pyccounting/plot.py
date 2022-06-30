3d1e2b4213da92677c688d93549eaecb94b8bd5e
clementjumel <clement.jumel@gmail.com>
2022-06-30 14:58:01 +0200

    :sparkles: Amount evolution display

---
+++ pyccounting/plot.py
@@ -0,0 +1,37 @@
+import datetime
+
+import pandas as pd
+
+
+def plot_account(
+    ax,
+    account: str,
+    start_amount: float,
+    df_account: pd.DataFrame,
+    min_date: datetime.date,
+    max_date: datetime.date,
+) -> None:
+    total_amount: float = start_amount
+    series_account: pd.Series = df_account.set_index("date")["amount"]
+
+    x, y = [min_date], [start_amount]
+    for date, operation_amount in series_account.items():
+        total_amount += operation_amount
+        x.append(date)
+        y.append(total_amount)
+    x.append(max_date)
+    y.append(total_amount)
+
+    ax.annotate(
+        text=round(y[0], 2),
+        xy=(min_date, y[0]),
+        xytext=(-50, 0),
+        textcoords="offset points",
+    )
+    ax.annotate(
+        text=round(y[-1], 2),
+        xy=(max_date, y[-1]),
+        xytext=(5, 0),
+        textcoords="offset points",
+    )
+    ax.plot(x, y, label=account)
--- pyccounting/streamlit_app.py
+++ pyccounting/streamlit_app.py
@@ -1,3 +1,52 @@
+import json
+
+import matplotlib.pyplot as plt
+import pandas as pd
 import streamlit as st
 
+from pyccounting.database import engine
+from pyccounting.plot import plot_account
+
 st.write("Welcome in Pyccounting.")
+
+df = pd.read_sql(sql="operation", con=engine)
+df = df[["date", "account", "amount"]]
+df = df.sort_values(by="date")
+df["date"] = df["date"].apply(lambda x: x.date())
+
+dates = df["date"].values
+min_date, max_date = min(dates), max(dates)
+
+accounts: list[str] = sorted(set(df["account"].values))
+display_accounts = [account for account in accounts if st.checkbox(account, value=True)]
+display_total = st.checkbox("total", value=True)
+
+with open("data/start.json") as file:
+    start_amounts = json.load(file)
+
+fig, ax = plt.subplots()
+for account in display_accounts:
+    plot_account(
+        ax=ax,
+        account=account,
+        start_amount=start_amounts[account]["amount"],
+        df_account=df.loc[df["account"] == account],
+        min_date=min_date,
+        max_date=max_date,
+    )
+if display_total:
+    plot_account(
+        ax=ax,
+        account="total",
+        start_amount=sum(start_amounts[account]["amount"] for account in accounts),
+        df_account=df,
+        min_date=min_date,
+        max_date=max_date,
+    )
+
+ax.axhline(y=0, color="k")
+ax.grid(True, which="both")
+ax.set_yticklabels([])
+plt.xlim(min_date, max_date)
+plt.legend()
+st.pyplot(fig=fig)
