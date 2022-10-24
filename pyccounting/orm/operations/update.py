9453634f19d86126ece7ef69f0115dd70347f01d
clementjumel <clement.jumel@gmail.com>
2022-10-25 00:41:47 +0200

    :adhesive_bandage: Fix the find category feature

--- pyccounting/orm/operations/update.py
+++ pyccounting/orm/operations/update.py
@@ -1,16 +1,22 @@
 from sqlalchemy.orm import Session
 
-from pyccounting.orm.data_model import Operation, Rule
+from pyccounting.orm.categories import get_category
+from pyccounting.orm.data_model import Category, Operation, Rule
 from pyccounting.orm.database import engine
 from pyccounting.orm.rules import get_rules
-
-from .get import get_category_operations
 
 
 def find_category() -> None:
     rules: list[Rule] = get_rules()
+    category: Category = get_category(category_name="unknown")
+
     with Session(engine) as session:
-        operations: list[Operation] = get_category_operations(category_name="unknown")
+        operations: list[Operation] = (
+            session.query(Operation)
+            .filter(Operation.category_id == category.id_)
+            .order_by(Operation.idx)
+            .all()
+        )
         for operation in operations:
             operation.find_category(rules=rules)
         session.commit()
