79156c156e06126e26ec876ab8dba1f0142c0505
clementjumel <clement.jumel@gmail.com>
2022-10-25 00:53:08 +0200

    :truck: Move importation to homepage

--- pyccounting/app.py
+++ pyccounting/app.py
@@ -1,17 +1,13 @@
-import datetime
-
 import pandas as pd
 import streamlit as st
 from streamlit.runtime.uploaded_file_manager import UploadedFile
 
-from pyccounting import initialize, orm, widgets
+from pyccounting import initialize, orm
 
 initialize.initialize()
 
-dates: tuple[datetime.date, datetime.date] = widgets.dates()
-accounts: list[str] = widgets.accounts(extended=False)
-types: list[str] = widgets.types(extended=False)
-category_names: list[str] = widgets.categories()
+st.title("Welcome in Pyccounting 😎")
+st.write(f"There are currently **{len(orm.get_operations())} operations** imported.")
 
 st.write("### Import Operations")
 
@@ -29,22 +25,11 @@ if uploaded_file is not None:
         df = df.iloc[::-1]  # reverse the order of the DataFrame
     else:
         raise ValueError
-    st.dataframe(df)
     orm.add_operations(df=df, account=account)
     orm.find_category()
     st.info(f"{len(df)} operations imported.")
 
-st.write("### Imported Operations")
-df = orm.get_operation_df(
-    accounts=accounts,
-    types=types,
-    date_index=True,
-    sort_by_date=True,
-    dates=dates,
-    category_names=category_names,
-)
-if df.empty:
-    st.write("No operation imported, yet.")
-else:
-    st.write(f"{len(df.index)} operations imported:")
+df = orm.get_operation_df(date_index=True, sort_by_date=True)
+if not df.empty:
+    st.write("### Imported Operations")
     st.dataframe(df)
--- pyccounting/streamlit_app.py
+++
@@ -1,7 +0,0 @@
-import streamlit as st
-
-from pyccounting import initialize
-
-initialize.initialize()
-
-st.write("Welcome in Pyccounting. 😎")
