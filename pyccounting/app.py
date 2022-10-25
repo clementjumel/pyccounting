f8e0c49f08bd644ccb39688566d69ff699df6358
clementjumel <clement.jumel@gmail.com>
2022-10-25 09:39:58 +0200

    :sparkles: Button to reboot the database

--- pyccounting/app.py
+++ pyccounting/app.py
@@ -1,8 +1,11 @@
+from pathlib import Path
+
 import pandas as pd
 import streamlit as st
 from streamlit.runtime.uploaded_file_manager import UploadedFile
 
 from pyccounting import initialize, orm
+from pyccounting._path import _ROOT
 
 initialize.initialize()
 
@@ -33,3 +36,9 @@ df = orm.get_operation_df(date_index=True, sort_by_date=True)
 if not df.empty:
     st.write("### Imported Operations")
     st.dataframe(df)
+
+    st.write("### Reboot Operations")
+    if st.button("Reboot"):
+        Path.unlink(_ROOT / "data" / "databases" / "sqlite.db")
+        st.info("Database re-booted")
+        initialize.initialize()
