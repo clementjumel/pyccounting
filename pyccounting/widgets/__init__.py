f9ab28ef94bde4a18c0199e6bfe908e9ff886ee1
clementjumel <clement.jumel@gmail.com>
2022-10-25 10:30:45 +0200

    :recycle: Improve widgets management

--- pyccounting/widgets/__init__.py
+++ pyccounting/widgets/__init__.py
@@ -1,4 +1,4 @@
 from .accounts import accounts_widget as accounts
 from .categories import categories_widget as categories
-from .dates import dates_widget as dates
+from .start_date import start_date_widget as start_date
 from .types import types_widget as types
